15a3b9ad23bf0529b1a7c37a9ec0bcdbdf4d3209
fanke186 <fanke186@163.com>
2020-10-22 13:18:06 +0800

    763：划分字符串

--- 题目/763.cpp
+++ 题目/763.cpp
@@ -30,3 +30,27 @@ public:
         return ret;
     }
 };
+
+
+// 把hash表改成数组
+// 大幅减少时间(非数量级)，小幅减少空间
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        // 找到每个字母的最大下标
+        int last[26] = {0};
+        for (int i=0; i<S.length(); i++)
+            last[S[i] - 'a'] = i;
+        //
+        int curStart = 0, curEnd = 0;
+        vector<int> ret;
+        for (int j=0; j<S.length(); j++){
+            curEnd = max(curEnd, last[S[j] - 'a']);
+            if (j == curEnd){
+                ret.push_back(curEnd - curStart + 1);
+                curStart = curEnd + 1;
+            }
+        }
+        return ret;
+    }
+};
