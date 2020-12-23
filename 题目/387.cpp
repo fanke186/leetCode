a0ea7e4c56a5d392dce65f86cc75ad08b568cb28
fanke186 <fanke186@163.com>
2020-12-23 11:17:42 +0800

    387：第一个不重复字母

---
+++ 题目/387.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        //vector<int> cnt(26, 0);
+        int cnt[26] = {0};
+        int ret = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            cnt[s[i] - 'a']++;
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (cnt[s[i] - 'a'] == 1) {
+                ret = i;
+                break;
+            }
+
+        }
+        return ret;
+    }
+};
